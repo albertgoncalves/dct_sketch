84ff7881e066e4355e754a666f4455f213811d4d
albertgoncalves <goncalves.albert@gmail.com>
2019-03-26 22:05:41 -0400

    organizing

--- src/utils.hs
+++ src/utils.hs
@@ -1,7 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
 
-import Control.Monad (join)
-import Data.List (intercalate)
+import Data.List (concat)
 import Text.Read (readMaybe)
 import Text.Regex (matchRegex, mkRegex, Regex)
 
@@ -10,16 +9,11 @@ scaleTo16bit x = (x', x', x')
   where
     x' = x * 257
 
-extract :: Maybe [String] -> Maybe [Int]
--- extract = sequence . map join . sequence . fmap (map readMaybe)
-extract = mapM join . traverse (map readMaybe)
-
-pattern :: Regex
-pattern = mkRegex p
+groups :: Regex
+groups = mkRegex p
   where
     p =
-        intercalate
-            ""
+        concat
             [ "([0-9]+),([0-9]+): +"
             , "\\([0-9]+,[0-9]+,[0-9]+\\) +"
             , "#[a-zA-Z0-9]+ "
@@ -27,9 +21,9 @@ pattern = mkRegex p
             ]
 
 extractRegex :: String -> Maybe [Int]
-extractRegex = extract . matchRegex pattern
+extractRegex = (mapM readMaybe =<<) . matchRegex groups
 
 main :: IO ()
 main = do
-    mapM_ (print . scaleTo16Bit) [0, 1, 120, 255]
+    mapM_ (print . scaleTo16bit) [0, 1, 120, 255]
     print $ extractRegex "0,1: (65535,65535,65535)  #FFFFFF  gray(255)"
