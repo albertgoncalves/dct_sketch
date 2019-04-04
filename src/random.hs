4ec89375a731c530cd4711783da4bde9afaff654
albertgoncalves <goncalves.albert@gmail.com>
2019-04-04 00:38:18 -0400

    shuffling progress

--- src/random.hs
+++ src/random.hs
@@ -1,28 +1,33 @@
 {-# OPTIONS_GHC -Wall #-}
 
-import Data.Word (Word64)
-import System.Random (Random, random)
+import Data.Function (on)
+import Data.List (partition)
+import System.Random (random, split)
 import System.Random.TF (TFGen, seedTFGen)
 
-type Seed = (Word64, Word64, Word64, Word64)
-type RandGen a = (a, TFGen)
+mapPair :: (a -> b) -> (a, a) -> (b, b)
+mapPair = uncurry . on (,)
 
-randInit :: (TFGen -> (a, TFGen)) -> Seed -> (a, TFGen)
-randInit g = g . seedTFGen
+randBools :: TFGen -> [(Bool, TFGen)]
+randBools g = iterate (random . snd) (random g)
 
-randFold :: Random a => (a, TFGen) -> (a, TFGen)
-randFold = random . snd
+partitionR :: [a] -> TFGen -> (([a], [a]), (TFGen, TFGen))
+partitionR xs g = (f (partition (fst . snd) ys), g')
+  where
+    ys = zip xs (randBools g)
+    f = mapPair (map fst)
+    g' = (split . snd . snd) (foldl1 (curry snd) ys)
 
-randIter
-    :: Random a => (RandGen a -> RandGen a)
-    -> Int
-    -> RandGen a
-    -> [RandGen a]
-randIter f n = take n . iterate f
+loop :: Int -> (a -> (b, (a, a))) -> (a, a) -> [b] -> [b]
+loop n f x accu
+    | n <= 0 = accu
+    | otherwise = loop (n - 1) f x' (xs:accu)
+  where
+    (xs, x') = f (snd x)
 
 main :: IO ()
-main = print $ (f 10) seed
+main = (putStr . unlines . map show) (loop n (partitionR xs) seed [])
   where
-    seed = (0, 0, 0, 0)
-    random' = random :: TFGen -> RandGen Bool
-    f n = map fst . randIter randFold n . randInit random'
+    n = 10
+    xs = [1 .. 10] :: [Int]
+    seed = split $ seedTFGen (0, 0, 0, 0)
--- src/shuffle.hs
+++ src/shuffle.hs
@@ -1,25 +1,35 @@
 {-# OPTIONS_GHC -Wall #-}
 
+import Data.Function (on)
+import Data.List (intercalate, partition)
 import System.Random (mkStdGen, setStdGen, randomIO)
 
--- https://hackage.haskell.org/package/extra-1.5.1/docs/src/Control.Monad.Extra.html#partitionM
-partitionM :: Monad m => (a -> m Bool) -> [a] -> m ([a], [a])
-partitionM _ [] = return ([], [])
-partitionM f (x:xs) =
-    f x >>= \y ->
-    partitionM f xs >>= \(a, b) ->
-    return ([x | y] ++ a, [x | not y] ++ b)
+mapPair :: (a -> b) -> (a, a) -> (b, b)
+mapPair = uncurry . on (,)
+
+attach :: a -> IO (Bool, a)
+attach x = (randomIO :: IO Bool) >>= \b -> return (b, x)
+
+partitionR :: [a] -> IO ([a], [a])
+partitionR xs = lr >>= \(l, r) -> return $ mapPair (map snd) (l, r)
+  where
+    lr = fmap (partition fst) (mapM attach xs)
 
 shuffle :: [a] -> IO [a]
 shuffle [] = return []
 shuffle [x] = return [x]
-shuffle xs =
-    partitionM (\_ -> randomIO :: IO Bool) xs >>= \(before, after) ->
-    shuffle before >>= \before' ->
-    shuffle after >>= return . (before' ++)
+shuffle xs = lr >>= \(l, r) -> shuffle l >>= \l' -> fmap (l' ++) (shuffle r)
+  where
+    lr = partitionR xs
+
+loop :: Monad m => Int -> m a -> m a
+loop n f
+    | n <= 0 = f
+    | otherwise = f >> loop (n - 1) f
 
 main :: IO ()
-main =
-    (setStdGen . mkStdGen) 2 >>
-    shuffle ([1 .. 25] :: [Int]) >>=
-    print
+main = (setStdGen . mkStdGen) 0 >> loop n f
+  where
+    n = 10 :: Int
+    xs' = [1 .. 5] :: [Int]
+    f = shuffle xs' >>= putStrLn . intercalate "," . map show
