c3bad6aacd1005157f97c6e5e789add58a0c62bf
albertgoncalves <goncalves.albert@gmail.com>
2019-04-04 09:07:27 -0400

    pure shuffle

--- src/random.hs
+++ src/random.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 
 import Data.Function (on)
-import Data.List (partition)
+import Data.List (intercalate, partition)
 import System.Random (random, split)
 import System.Random.TF (TFGen, seedTFGen)
 
@@ -11,23 +11,33 @@ mapPair = uncurry . on (,)
 randBools :: TFGen -> [(Bool, TFGen)]
 randBools g = iterate (random . snd) (random g)
 
-partitionR :: [a] -> TFGen -> (([a], [a]), (TFGen, TFGen))
-partitionR xs g = (f (partition (fst . snd) ys), g')
+partitionR :: [a] -> TFGen -> (([a], TFGen), ([a], TFGen))
+partitionR xs g = ((ls, lg), (rs, rg))
   where
     ys = zip xs (randBools g)
     f = mapPair (map fst)
-    g' = (split . snd . snd) (foldl1 (curry snd) ys)
+    (lg, rg) = (split . snd . snd) (foldl1 (curry snd) ys)
+    (ls, rs) = f (partition (fst . snd) ys)
 
-loop :: Int -> (a -> (b, (a, a))) -> (a, a) -> [b] -> [b]
+shuffle :: ([a], TFGen) -> ([a], TFGen)
+shuffle ([], g) = ([], g)
+shuffle ([x], g) = ([x], g)
+shuffle (xs, g) = (ls' ++ rs', rg')
+  where
+    ((ls', _), (rs', rg')) = mapPair shuffle $ partitionR xs g
+
+loop :: Int -> (a -> (b, a)) -> a -> [b] -> [b]
 loop n f x accu
     | n <= 0 = accu
     | otherwise = loop (n - 1) f x' (xs:accu)
   where
-    (xs, x') = f (snd x)
+    (xs, x') = f x
 
 main :: IO ()
-main = (putStr . unlines . map show) (loop n (partitionR xs) seed [])
+main = pprint ys
   where
     n = 10
-    xs = [1 .. 10] :: [Int]
-    seed = split $ seedTFGen (0, 0, 0, 0)
+    xs = [1 .. 5] :: [Int]
+    seed = seedTFGen (0, 0, 0, 0)
+    ys = loop n (curry shuffle xs) seed []
+    pprint = putStr . unlines . map (intercalate "," . map show)
